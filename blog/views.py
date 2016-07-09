1ecf2d98b34a1ec918cd4e27c13c3e2577c95306
Mark Dawson <medawson94@gmail.com>
2016-07-10 00:25:08 +0400

    adding email sharing

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
+
 
 class PostListView(ListView):
 	queryset = Post.published.all()
@@ -35,3 +38,26 @@ def post_detail(request, year, month, day, post):
 	return render(request,
 				'blog/post/detail.html',
 				{'post':post})
+
+def post_share(request, post_id):
+	"""
+	Retrieve post by id
+	"""
+	post = get_object_or_404(Post, id=post_id, status='published')
+	sent = False
+	if request.method == 'POST':
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you read "{}"'.format(
+				cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+				post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+			sent = True
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/share.html', {'post':post,
+															'form':form,
+															'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,15 +12,23 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment varaible" % var_name
+        raise 
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-scier59lq7ov_cpww-(4(aa(-i3i9yjpdwu#b&-mc4^8+ma54'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email Hosting
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
